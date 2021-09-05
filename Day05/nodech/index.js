d82ab9b9471cb3782f5692f441c8e8b7f6f6d13b
Viniket <sviniket1201@gmail.com>
2021-09-05 18:18:04 +0530

    completed day1

---
+++ Day05/nodech/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+// fs.mkdirSync("viniket");
+
+// fs.writeFileSync('viniket/devs.txt',"my name is viniket");
+
+// fs.writeFileSync('viniket/devs.txt',"my name is viniket saraf");
+
+// fs.appendFileSync('viniket/devs.txt',"my name is viniket saraf....");
+
+// const data = fs.readFileSync('viniket/devs.txt', 'utf8');
+// console.log(data);
+
+// fs.renameSync('viniket/devs.txt','viniket/devsnest.txt');
+
+// fs.unlinkSync('viniket/devsnest.txt');
+
+// fs.rmdirSync('viniket');
